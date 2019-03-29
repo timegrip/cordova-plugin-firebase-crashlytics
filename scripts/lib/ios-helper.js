624be2872be71b005e9a3fffbc540c746cfa28e9
Zarko Hristovski <zarko@timegrip.no>
2019-03-29 10:48:46 +0100

    One more attempt

--- scripts/lib/ios-helper.js
+++ scripts/lib/ios-helper.js
@@ -34,9 +34,7 @@ module.exports = {
             isa: "PBXShellScriptBuildPhase",
             buildActionMask: 2147483647,
             files: [],
-            inputPaths: [
-                '"$(BUILT_PRODUCTS_DIR)/$(INFOPLIST_PATH)"'
-            ],
+            inputPaths: [],
             name: comment,
             outputPaths: [],
             runOnlyForDeploymentPostprocessing: 0,
